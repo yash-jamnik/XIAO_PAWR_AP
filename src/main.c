e57a369c28a7e27d8bbb9c1e889babcdecde3b30
Yash <yash.j@intellobots.com>
2026-07-29 17:18:57 +0530

    changed the version

--- src/main.c
+++ src/main.c
@@ -1644,7 +1644,7 @@ static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 	memset(name, 0, sizeof(name));
 	bt_data_parse(ad, data_cb, name);
 
-	if (strcmp(name, "PAwR sync sample"))
+	if (strcmp(name, "PAWR_SYNC_SAMPLE"))
 		return;
 
 	/* Controller cooldown protection */
